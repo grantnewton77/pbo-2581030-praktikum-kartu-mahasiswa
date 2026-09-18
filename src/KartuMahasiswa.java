b2bd3e8553051478bdc5faaaea4108715fdfbefe
Grant Newton <2581030@unai.edu>
2026-09-18 09:30:37 +0700

    Menambahkan Nama Lengkap

--- src/KartuMahasiswa.java
+++ src/KartuMahasiswa.java
@@ -20,18 +20,19 @@ public class KartuMahasiswa {
         System.out.print("Masukkan Status Pernikahan : ");
         boolean sudahMenikah = input.nextBoolean();
 
+        input.nextLine();
 
-
+        System.out.print("Masukkan Nama lengkap : ");
+        String namaLengkap = input.nextLine();
 
         System.out.println();
         System.out.println("===== KARTU MAHASISWA =====");
-
+        System.out.println("Nama        : " + namaLengkap);
         System.out.println("NIK         : " + nik);
         System.out.println("Umur        : " + umur + " tahun");
         System.out.println("Tinggi      : " + tinggiBadan + " cm");
         System.out.println("Gol darah   : " + golonganDarah);
         System.out.println("Menikah     : " + sudahMenikah);
-
 
         input.close();
     }
