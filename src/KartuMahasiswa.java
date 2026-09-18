103b68551d165aa5f248241a5261c2b52a673018
Grant Newton <2581030@unai.edu>
2026-09-18 09:26:45 +0700

    Menambahkan Golongan Darah

---
+++ src/KartuMahasiswa.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class KartuMahasiswa {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan NIK : ");
+        long nik = input.nextLong();
+
+        System.out.print("Masukkan Umur : ");
+        int umur = input.nextInt();
+
+        System.out.print("Masukkan Tinggi Badan : ");
+        double tinggiBadan = input.nextDouble();
+
+        System.out.print("Masukkan Golongan Darah : ");
+        char golonganDarah = input.next().charAt(0);
+
+
+
+
+
+        System.out.println();
+        System.out.println("===== KARTU MAHASISWA =====");
+
+        System.out.println("NIK         : " + nik);
+        System.out.println("Umur        : " + umur + " tahun");
+        System.out.println("Tinggi      : " + tinggiBadan + " cm");
+        System.out.println("Gol darah   : " + golonganDarah);
+
+
+        input.close();
+    }
+}
